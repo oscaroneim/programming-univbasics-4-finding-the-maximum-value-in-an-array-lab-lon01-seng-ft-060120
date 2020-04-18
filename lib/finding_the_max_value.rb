646f3a92775624626c0207cb44784eaaf2653aa2
Oscar Oneim <oscar4londres@gmail.com>
2020-04-18 19:36:31 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,3 +2,15 @@ def find_max_value(array)
  array.sort!
  return array.last
 end
+
+def find_max_value(array)
+  count = 0
+  max_value = -1
+  while count < array.length do
+    if max_value < array[count]
+      max_value = array[count]
+    end
+    count += 1
+  end
+  max_value
+end
